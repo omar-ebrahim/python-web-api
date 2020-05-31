0569f3b8441d7fb7cd171c72708f6d6db48c1847
Omar Ebrahim <omar.plym@gmail.com>
2020-05-31 11:51:12 +0100

    corrected

--- api/api.py
+++ api/api.py
@@ -4,13 +4,6 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-# Test data
-books = [
-    {
-        "id:" 1,
-    }
-]
-
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Hello world!</h1>"
