c394cdc8d0e3951fac2404ff2aa1e482e7efe122
Omar Ebrahim <omar.plym@gmail.com>
2020-06-01 07:03:43 +0100

    Added DELETE

--- api/api.py
+++ api/api.py
@@ -30,25 +30,40 @@ def all_manufacturers():
     elif request.method == 'POST':
         data_to_insert = request.form
         row_to_insert = manufacturer.Manufacturer(
-            data_to_insert['_id'], data_to_insert['name'], data_to_insert['abbreviation']
+            int(data_to_insert['_id']
+                ), data_to_insert['name'], data_to_insert['abbreviation']
         )
-        inserted_row = databaseaccess.DatabaseAccess.manufacturers_coll.insert_one(row_to_insert.toObject())
-        return_data = { '_id': inserted_row.inserted_id }
-        resp = Response(json.dumps(return_data), status=200, mimetype='application/json')
+        inserted_row = databaseaccess.DatabaseAccess.manufacturers_coll.insert_one(
+            row_to_insert.toObject())
+        return_data = {'_id': inserted_row.inserted_id}
+        resp = Response(json.dumps(return_data), status=200,
+                        mimetype='application/json')
         return resp
     else:
         return abort(405)
 
 
-@app.route('/api/manufacturers/<int:id>/', methods=['GET'])
+@app.route('/api/manufacturers/<int:id>', methods=['GET', 'DELETE'])
 def manufacturer_by_id(id):
-    mf = databaseaccess.DatabaseAccess.manufacturers_coll.find_one({"_id": id})
-    if mf is None:
-        return abort(404, description="The manufacturer with id " + str(id) + " does not exist")
+
+    if request.method == 'GET':
+        mf = databaseaccess.DatabaseAccess.manufacturers_coll.find_one({
+                                                                       "_id": id})
+        if mf is None:
+            return abort(404, description="The manufacturer with id " + str(id) + " does not exist")
+        else:
+            row = manufacturer.Manufacturer(
+                mf['_id'], mf['name'], mf['abbreviation'])
+            return jsonify(row.toObject())
+    elif request.method == 'DELETE':
+        deleted_row = databaseaccess.DatabaseAccess.manufacturers_coll.delete_one({
+                                                                                  "_id": id})
+        return_data = {'deleted': deleted_row.deleted_count}
+        resp = Response(json.dumps(return_data), status=200,
+                        mimetype='application/json')
+        return resp
     else:
-        row = manufacturer.Manufacturer(
-            mf['_id'], mf['name'], mf['abbreviation'])
-        return jsonify(row.toObject())
+        return abort(405)
 
 
 app.run()  # this wil run on 127.0.0.1 - not accessible over network
